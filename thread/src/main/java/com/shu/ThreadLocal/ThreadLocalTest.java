83db048422599284a1360e8ab223d0caccc7d94a
shuyunxiang <572130390@qq.com>
2023-02-28 08:40:49 +0800

    TransmittableThreadLocal引入

---
+++ thread/src/main/java/com/shu/ThreadLocal/ThreadLocalTest.java
@@ -0,0 +1,16 @@
+package com.shu.ThreadLocal;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+
+public class ThreadLocalTest {
+    public static void main(String[] args) {
+        ThreadLocal<String> stringThreadLocal = new ThreadLocal<>();
+        stringThreadLocal.set("1");
+        String s = stringThreadLocal.get();
+        System.out.println(s);
+        stringThreadLocal.remove();
+
+        TransmittableThreadLocal transmittableThreadLocal = new TransmittableThreadLocal();
+    }
+
+}
